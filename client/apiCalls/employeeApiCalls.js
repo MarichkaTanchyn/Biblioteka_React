b1298878794279f1c789c1f67e54464897770b07
Maria <mariatanshyn@gmail.com>
2022-01-16 16:28:20 +0100

    Deleted some packages

--- client/apiCalls/employeeApiCalls.js
+++
@@ -1,6 +0,0 @@
-const employeeBaseURL = "http://localhost:3000/api/employees";
-
-export function getEmployeeApiCall(){
-    const promise = fetch(employeeBaseURL);
-    return promise;
-}
--- client/apiCalls/employmentApiCall.js
+++
@@ -1,6 +0,0 @@
-const employmentBaseURL = "http://localhost:3000/api/employments";
-
-export function getEmploymentApiCall(){
-    const promise = fetch(employmentBaseURL);
-    return promise;
-}
